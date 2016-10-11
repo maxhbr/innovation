3facb00e246ab32853b21191e674fc4d01189ac2
maximilianhuber <maximilian.huber@tngtech.com>
2016-10-11 13:30:09 +0200

    main should tell the current git information

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
+import Development.GitRev
+
+gitInfo :: IO ()
+gitInfo = putStrLn (concat [ "[", $(gitBranch), "@", $(gitHash)
+                           , " (", $(gitCommitDate), ")"
+                           , " (", $(gitCommitCount), " commits in HEAD)"
+                           , dirty, "] "])
+  where
+    dirty | $(gitDirty) = " (unclean)"
+          | otherwise   = ""
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = gitInfo
